78c33a4f22c0fb84f53d19d10cb205f12e5fa48a
Raphael L <raphaleiva@gmail.com>
2026-07-28 21:23:07 -0300

    fix(retrospective): add default values for FromDate and ToDate query parameters

--- Features/Retrospective/Presentation/Dtos/GetMyRetrospective/GetMyRetrospectiveRequestDto.cs
+++ Features/Retrospective/Presentation/Dtos/GetMyRetrospective/GetMyRetrospectiveRequestDto.cs
@@ -1,6 +1,7 @@
 namespace EbenezerBackend.Features.Retrospective.Presentation.Dtos.GetMyRetrospective;
 
-public record GetMyRetrospectiveRequestDto(
-    DateTime FromDate,
-    DateTime ToDate
-    );
+public record GetMyRetrospectiveRequestDto
+{
+    public DateTime FromDate { get; init; } = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    public DateTime ToDate { get; init; } = DateTime.UtcNow;
+}
